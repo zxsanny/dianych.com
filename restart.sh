fb50f6ff6bbf27d08109cc71f579aa00d252c359
Oleksandr Bezdieniezhnykh <zxsanny@gmail.com>
2025-09-15 10:11:03 +0300

    remove data files update nginx config and update restart scripts with correct image location

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 docker stop dianych.com
 docker rm dianych.com
 docker run -p 3001:3000 \
-          -v /root/dianych/images:/app/public/images \
+          -v /var/www/dianych/images:/app/public/images \
           -e SECRET_COOKIE_PASSWORD="$(openssl rand -base64 48)" \
           --name dianych.com --restart always docker.azaion.com/dianych
--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@ docker rm dianych.com
 docker login docker.azaion.com
 docker pull docker.azaion.com/dianych:latest
 docker run -p 3001:3000 \
-          -v /root/dianych/images:/app/public/images \
-          -e SECRET_COOKIE_PASSWORD="$(openssl rand -base64 48)" \
+          -v /var/www/dianych/images:/app/public/images \
+          -e SECRmET_COOKIE_PASSWORD="$(openssl rand -base64 48)" \
           --name dianych.com --restart always docker.azaion.com/dianych
